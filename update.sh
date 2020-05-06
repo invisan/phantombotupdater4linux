09997ec9ee4ef2e71bb3bb2cc535b3c71823a946
invisan <da_invisible@freenet.de>
2020-05-06 18:19:41 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 # !/bin/bash
 
+# Function to check the Version 
+function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
+
+
+
 installed=$(unzip -p phantombot/PhantomBot.jar "META-INF/MANIFEST.MF" | grep -oP "(?<=Implementation-Version: )(\d+|\.)*")
 wget https://raw.githubusercontent.com/PhantomBot/PhantomBot/master/build.xml
 newest=$(grep -Po '<property name="version".*?value="\K[^"]*' build.xml)
@@ -7,10 +12,10 @@ echo "Installed Version is:" $installed
 echo "Newest Version is:" $newest
 rm build.xml
 
-if [ "$installed" = "$newest" ]; then
-        echo "Same Version already installed."
+if [ $(version $installed) -ge $(version $newest) ]; then
+        echo "Newest Version already installed."
 else
-        echo "Different Version found. Downloading now."
+        echo "Newer Version found. Downloading now."
 
 # Download of the latest Version of PhantomBot from Github
 curl -s https://api.github.com/repos/PhantomBot/PhantomBot/releases/latest \
