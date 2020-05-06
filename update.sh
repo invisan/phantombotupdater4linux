0cb8e01284078680ac300b337e0cdc541607e93a
invisan <da_invisible@freenet.de>
2020-05-06 17:57:07 +0200

    Update update.sh
    Update is now able to see if the current Version is equal to the newest Version online.
    Will be changed to fit Versions with double digits at the end (example 3.1.10).

--- update.sh
+++ update.sh
@@ -1,11 +1,24 @@
 # !/bin/bash
 
+installed=$(unzip -p phantombot/PhantomBot.jar "META-INF/MANIFEST.MF" | grep -oP "(?<=Implementation-Version: )(\d+|\.)*")
+wget https://raw.githubusercontent.com/PhantomBot/PhantomBot/master/build.xml
+newest=$(grep -Po '<property name="version".*?value="\K[^"]*' build.xml)
+echo "Installed Version is:" $installed
+echo "Newest Version is:" $newest
+rm build.xml
+
+if [ "$installed" = "$newest" ]; then
+        echo "Same Version already installed."
+else
+        echo "Different Version found. Downloading now."
+
 # Download of the latest Version of PhantomBot from Github
 curl -s https://api.github.com/repos/PhantomBot/PhantomBot/releases/latest \
 | grep "browser_download_url.*zip" \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi -
+
 
 # Remove the old phantombot-old Folder
 rm -R ./phantombot-old
@@ -32,3 +45,5 @@ chmod u+x ./phantombot/java-runtime-linux/bin/java
 
 # Remove the Zip File
 rm PhantomBot*.zip
+
+fi
