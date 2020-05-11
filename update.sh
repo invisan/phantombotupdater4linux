8b826e1cd7b54556cdd20b8c1cf0e3a0f4683922
invisan <da_invisible@freenet.de>
2020-05-11 15:56:23 +0200

    Update to v0.5

--- update.sh
+++ update.sh
@@ -1,16 +1,59 @@
 # !/bin/bash
 
+if [ -z "$1" ]; then
+       echo "Config file not set. Using sample.cfg"
+        conf="sample.cfg"
+else
+        conf=$1
+        echo "Config found."
+fi
+
+# Check if conf file present
+if [ -f "$conf" ]; then
+        echo "File exists. Continuing."
+else
+        echo "File not found. Exiting."
+        exit
+fi
+
+
 # Function to check the Version 
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
+# Grepping the variables from the conf File.
+home=$(grep -Po 'home="\K[^"]*' $conf)
+path=$(grep -Po 'path="\K[^"]*' $conf)
+user=$(grep -Po 'user="\K[^"]*' $conf)
+build=$(grep -Po 'build="\K[^"]*' $conf)
+port=$(grep -Po 'port="\K[^"]*' $conf)
 
 
-installed=$(unzip -p phantombot/PhantomBot.jar "META-INF/MANIFEST.MF" | grep -oP "(?<=Implementation-Version: )(\d+|\.)*")
+installed=$(unzip -p $path/PhantomBot.jar "META-INF/MANIFEST.MF" | grep -oP "(?<=Implementation-Version: )(\d+|\.)*")
+
+if [ $build = "stable" ]; then
 wget https://raw.githubusercontent.com/PhantomBot/PhantomBot/master/build.xml
 newest=$(grep -Po '<property name="version".*?value="\K[^"]*' build.xml)
+rm ./build.xml
+else
+        if [ $build = "nightly" ]; then
+                wget https://github.com/PhantomBot/nightly-build/raw/master/PhantomBot-nightly-lin.zip -O phantombot.zip
+                unzip ./phantombot.zip
+                mv ./PhantomBot* nightly
+                newest=$(unzip -p nightly/PhantomBot.jar "META-INF/MANIFEST.MF" | grep -oP "(?<=Implementation-Version: )(\d+|\.|\-|\d+|\-\d+)*")
+        else
+                if [ $build = "pbotde" ]; then
+                        wget https://raw.githubusercontent.com/PhantomBotDE/PhantomBotDE/master/build.xml
+                        newest=$(grep -Po '<property name="version".*?value="\K[^"]*' build.xml)
+                        rm ./build.xml
+                else
+                        echo "No config found."
+                fi
+        fi
+fi
+
+
 echo "Installed Version is:" $installed
 echo "Newest Version is:" $newest
-rm build.xml
 
 if [ $(version $installed) -ge $(version $newest) ]; then
         echo "Newest Version already installed."
@@ -18,90 +61,98 @@ else
         echo "Newer Version found. Downloading now."
 
 # Download of the latest Version of PhantomBot from Github
-curl -s https://api.github.com/repos/PhantomBot/PhantomBot/releases/latest \
-| grep "browser_download_url.*zip" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi -
+if [ $build = "stable" ]; then
+
+        curl -s https://api.github.com/repos/PhantomBot/PhantomBot/releases/latest \
+        | grep "browser_download_url.*zip" \
+        | cut -d : -f 2,3 \
+        | tr -d \" \
+        | wget -qi - -O phantombot.zip
+
+else
+        if [ $build = "pbotde" ]; then
+
+        curl -s https://api.github.com/repos/PhantomBotDE/PhantomBotDE/releases/latest \
+        | grep "browser_download_url.*zip" \
+        | cut -d : -f 2,3 \
+        | tr -d \" \
+        | wget -qi - -O phantombot.zip
+
+
+        else
+                echo "Neither pbotde or stable specified. Must be nightly or something else."
+        fi
+fi
+
 
 # Check if PhantomBot is running
-running=$(ps -ef | grep '[P]hantomBot' | awk '{print $2}')
+running=$(lsof -t -i :$port -s TCP:LISTEN)
 
 if [ -z "$running" ]; then
                 echo "Bot seems to be not running. Continuing with update."
 else
-                # Check if PhantomBot Service is installed
-                service="/etc/systemd/system/phantombot.service"
-                if [ -f "$service" ]; then
-                        echo "Pid found. Stopping Bot."
-                        systemctl stop phantombot &>> ./updater.log
-                        sleep 15
-                else
-                        echo "Service not installed. Continuing with kill."
-                fi
+                echo "Pid found. Stopping Bot."
+                systemctl stop phantombot
                 # Check again if Bot is still running
-                running=$(ps -ef | grep '[P]hantomBot' | awk '{print $2}')
+                running=$(lsof -t -i :$port -s TCP:LISTEN)
                         if [ -z "$running" ]; then
                                 echo "Bot stopped. Continuing with update."
                         else
-                                echo "Seems service wasnt running or systemctl wasnt working. Trying kill."
+                                echo "Seems systemctl wasnt working. Trying kill."
                                 kill $(pgrep -f PhantomBot)
-                                echo "Waiting for 15 Seconds to check if the Bot is stopped."
-                                sleep 15
                                 # Check again if Bot is still running
-                                running=$(ps -ef | grep '[P]hantomBot' | awk '{print $2}')
+                                running=$(lsof -t -i :$port -s TCP:LISTEN)
                                         if [ -z "$running" ]; then
                                                 echo "Bot stopped. Continuing with update."
                                         else
                                                 echo "Seems kill did not work. Trying skill -9."
                                                 skill -9 $running
-                                                sleep 15
-                                                echo "Waiting for 15 Seconds to check if the Bot has stopped."
-                                                running=$(ps -ef | grep '[P]hantomBot' | awk '{print $2}')
-                                                        if [ -z "$running" ]; then
-                                                                echo "Bot stopped. Continuing with update."
-                                                        else
-                                                                FILE="./updater.log"
-                                                                        if [ -f "$FILE" ]; then
-                                                                                echo "File exists. Continuing." >> /dev/null
-                                                                        else
-                                                                                touch ./updater.log
-                                                                        fi;
-                                                                        datum=$(date +'%x %X')
-                                                                        log=$(echo " Seems the Bot could not be stopped succesfully. Exiting Script. Please stop the Bot manually. PID of the Process is: $running")
-                                                                        string=$datum$log
-                                                                        echo $string >> ./updater.log
-                                                                exit
-                                                        fi
                                         fi
                         fi
 fi
 
+if [ -d "$path" ]; then
 
-# Remove the old phantombot-old Folder
-rm -R ./phantombot-old
+        if [ -d "$path-old" ]; then
+
+                # Remove the old phantombot-old Folder
+                rm -R ./$path-old
+
+        fi
 
 # Move phantombot Folder to phantombot-old
-mv ./phantombot phantombot-old
+mv ./$path $path-old
+
+fi
+
+if [ $build = "nightly" ]; then
+
+        mv nightly $path
+
+else
 
 # Unzip the Zip File 
-unzip PhantomBot*.zip
+unzip phantombot.zip
 
 # "Rename" the extracted Folder through Move
-mv ./PhantomBot*/ phantombot
+mv ./PhantomBot*/ $path
+
+fi
+
+if [ -d "$path-old" ]; then
 
 # Copy the Old Configs
-cp -R ./phantombot-old/config/ ./phantombot/
-cp -R ./phantombot-old/scripts/lang/custom/ ./phantombot/scripts/lang/
+cp -R ./$path-old/config/ ./$path/
+cp -R ./$path-old/scripts/lang/custom/ ./$path/scripts/lang/
 
-# Change to the phantombot Folder to make the sh files executable
-cd phantombot
+fi
 
 # Make the sh files executable
-chmod u+x launch-service.sh launch.sh
-chmod u+x ./phantombot/java-runtime-linux/bin/java
+chmod u+x $home/$path/launch-service.sh
+chmod u+x $home/$path/launch.sh
+chmod u+x $home/$path/java-runtime-linux/bin/java
 
 # Remove the Zip File
-rm PhantomBot*.zip
+rm ./phantombot.zip
 
 fi
